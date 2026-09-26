08669009733d1d78d1d057ed597b685d4699e98b
gorillaznumberonefan-pixel <gorillaznumberonefan@icloud.com>
2026-09-26 08:59:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function search() {
 
     alert("You searched for: " + query);
 }
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    const button = document.getElementById("theme-toggle");
+
+    if (document.body.classList.contains("dark")) {
+        button.textContent = "☀️ Light Mode";
+    } else {
+        button.textContent = "🌙 Dark Mode";
+    }
+}
